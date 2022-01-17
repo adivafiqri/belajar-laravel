2f0f03cba4ffb3292c9da92b54ff42e5d8bece5d
adivafiqri <adivafiqri@gmail.com>
2022-01-17 11:56:48 +0700

    Menambahkan laravel ke git

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>Halaman HomeeS</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "home"
+    ]);
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "about",
+        "name" => "Adiva Fiqri",
+        "email" => "adivafiqrinugraha69@gmail.com",
+        "image" => "adiva.jpg"
+    ]);
+});
+
+
+
+
+Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post pertama",
+            "author" => "Adiva Fiqri",
+            "slug" => "judul-post-pertama",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est aut impedit molestiae, id aliquam facilis pariatur nam quae provident quis quod porro velit, perferendis consequuntur ipsam quidem. Fugiat quas expedita assumenda quia, autem possimus aliquid reiciendis sapiente mollitia nisi at odit obcaecati consectetur reprehenderit iusto repudiandae voluptatem rerum, explicabo nemo numquam vitae. Eius, nostrum dolorum? Quis laudantium ipsum esse similique. Libero, iure deleniti eos impedit iusto accusamus quod architecto corrupti commodi sit ipsum. Culpa placeat earum rem velit impedit voluptas."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "author" => "Akhmad Rizal",
+            "slug" => "judul-post-kedua",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est aut impedit molestiae, id aliquam facilis pariatur nam quae provident quis quod porro velit, perferendis consequuntur ipsam quidem. Fugiat quas expedita assumenda quia, autem possimus aliquid reiciendis sapiente mollitia nisi at odit obcaecati consectetur reprehenderit iusto repudiandae voluptatem rerum, explicabo nemo numquam vitae. Eius, nostrum dolorum? Quis laudantium ipsum esse similique. Libero, iure deleniti eos impedit iusto accusamus quod architecto corrupti commodi sit ipsum. Culpa placeat earum rem velit impedit voluptas."
+        ]
+    ];
+    return view('posts', [
+        "title" => "blog",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+//halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    return view('post');
+});
